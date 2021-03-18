4f8f3c72e628190ec9e24a204a921bba8b1e038c
Lucie <44549123+Luss7@users.noreply.github.com>
2021-03-18 09:12:52 +0100

    Nettoyage code

---
+++ Chatbot/vectorisation.py
@@ -0,0 +1,12 @@
+import spacy
+
+dir='D:/Documents/ENSC/GitHub/AlBidert/Eliza/';
+def path_to_text(path):
+    with open(path, 'r') as file_in:
+        # on récupère le contenu du fichier texte
+        return file_in.read();
+
+nlp = spacy.load('en_core_web_md');
+doc = nlp(path_to_text(dir+'dialogue0.txt'));
+print(doc.vector);
+print(len(doc.vector));
